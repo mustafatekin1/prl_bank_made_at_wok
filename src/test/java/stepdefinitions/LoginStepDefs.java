4f050e63d1f32918678f0c2b508e4790ea7c37a4
mustafatekin1 <valid.testio@gmail.com>
2021-11-23 17:50:13 +0300

    signinbutton click not working

--- src/test/java/stepdefinitions/LoginStepDefs.java
+++ src/test/java/stepdefinitions/LoginStepDefs.java
@@ -8,7 +8,6 @@ import utilities.Driver;
 public class LoginStepDefs {
 
     LoginPage loginPage = new LoginPage();
-
     @Given("user opens the url")
     public void user_opens_the_url() {
         Driver.getDriver().get(ConfigReader.getProperty("project_url"));
@@ -18,17 +17,17 @@ public class LoginStepDefs {
     loginPage.accountMenu.click();
     loginPage.signInMenu.click();
     }
-    @When("user enters username {customer_username}")
+    @When("user enters username {string}")
     public void user_enters_username(String customer_username) {
-    loginPage.username.sendKeys(customer_username);
+        loginPage.username.sendKeys(customer_username);
     }
-    @When("user enters password {customer_password}")
+    @When("user enters password {string}")
     public void user_enters_password(String customer_password) {
-    loginPage.password.sendKeys(customer_password);
+        loginPage.password.sendKeys(customer_password);
     }
     @Then("verify that user successfully logins to the account")
     public void verify_that_user_successfully_logins_to_the_account() {
-    loginPage.signInButton.click();
+        loginPage.signInButton.click();
     }
 
 
